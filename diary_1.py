959968f1cb0536e0232bddb1dba0eac1e2c5bd5b
user <danik.best0401@gmail.com>
2020-02-06 13:47:56 +0200

    06022020_13_47

---
+++ diary_1.py
@@ -0,0 +1,34 @@
+Python 3.6.8 (default, Jan 14 2019, 11:02:34) 
+[GCC 8.0.1 20180414 (experimental) [trunk revision 259383]] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> python
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    python
+NameError: name 'python' is not defined
+>>> print(4)
+4
+>>> type ('Hello,world!') <class 'str'>
+SyntaxError: invalid syntax
+>>> type ('Hello,world!')
+<class 'str'>
+>>> type(17)
+<class 'int'>
+>>> type(3.2)
+<class 'float'>
+>>> print(1,000,000)
+1 0 0
+>>> message = 'And now for something completely different'
+>>> n = 17
+>>> pi = 3.1415926535897931
+>>> print(n)
+17
+>>> print(pi)
+3.141592653589793
+>>> type(message)
+<class 'str'>
+>>> type(n)
+<class 'int'>
+>>> type(pi)
+<class 'float'>
+>>> 
